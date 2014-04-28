19ddf006b5e64f3a2209e2302c08da8da042013e
Mike 'Fuzzy' Partin <fuzzy@fu-manchu.org>
2014-04-28 14:30:46 -0700

    used a few meta techniques to support cleaner console output as well as background color support. documentation updates forthcoming.

--- lib/easy-color.rb
+++ lib/easy-color.rb
@@ -29,39 +29,49 @@
 # POSSIBILITY OF SUCH DAMAGE.
 ################################################################################
 
+$color     = {
+  :black   => 30,    :red       => 31,    :green     => 32,
+  :yellow  => 33,    :blue      => 34,    :purple    => 35,
+  :cyan    => 36,    :white     => 37
+}
+$attrs     = {
+  :clean   => 0,     :bold      => 1,     :underline => 4,
+  :blink   => 5,     :reverse   => 7,     :conceal   => 8
+}
+
 class String
 
-  # Normal colors
-  def black;       colorize(self, "\e[0m\e[30");     end
-  def red;         colorize(self, "\e[0m\e[31");     end
-  def green;       colorize(self, "\e[0m\e[32");     end
-  def yellow;      colorize(self, "\e[0m\e[33");     end
-  def blue;        colorize(self, "\e[0m\e[34");     end
-  def purple;      colorize(self, "\e[0m\e[35");     end
-  def cyan;        colorize(self, "\e[0m\e[36");     end
-  def white;       colorize(self, "\e[0m\e[37");     end
+  def valid(args)
+    if args.keys.include? :color and $color.keys.include? args[:color]
+      true
+    elsif args.keys.include? :attr and $attrs.keys.include? args[:attr]
+      true
+    else
+      false
+    end
+  end      
 
-  # Fun stuff
-  def clean;       colorize(self, "\e[0");           end
-  def bold;        colorize(self, "\e[1");           end
-  def underline;   colorize(self, "\e[4");           end
-  def blink;       colorize(self, "\e[5");           end
-  def reverse;     colorize(self, "\e[7");           end
-  def conceal;     colorize(self, "\e[8");           end
-
-  # Blanking
-  def clear_scr;   colorize(self, "\e[2", mode="J"); end
-
-  # Placement
-  def place(line, column)
-    colorize(self, "\e[#{line};#{column}", mode='f')
+  def fg(color)
+    "\033[#{$color[color]}m#{self}\033[0m" if valid(:color => color)
   end
 
-  def save_pos;    colorize(self, "\e[", mode='s');  end
-  def return_pos;  colorize(self, "\e[", mode='u');  end
+  def bg(color)
+    "\033[#{($color[color]+10)}m#{self}\033[0m" if valid(:color => color)
+  end
 
-  def colorize(text, color_code, mode='m')
-    "#{color_code}#{mode}#{text}\e[0m"
+  def attr(attr)
+    "\033[#{($attrs[attr])}m#{self}\033[0m" if valid(:attr => attr)
+  end
+
+  def method_missing(m, *args, &block)
+    if $color.keys.include?(m)
+      fg(m)
+    elsif $attrs.keys.include?(m)
+      attr(m)
+    else
+      raise NoMethodError, "Undefined method '#{m.inspect}'."
+    end
   end
 
 end
+
